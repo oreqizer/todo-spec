1c2e0847ae5a7d1149bf9d7b2dfea81e44ed74c5
oreqizer <oreqizer@gmail.com>
2024-01-13 15:46:30 +0100

    init

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+const path = require('node:path');
+
+const project = path.join(process.cwd(), 'tsconfig.json');
+
+module.exports = {
+  extends: [
+    require.resolve('@vercel/style-guide/eslint/node'),
+    require.resolve('@vercel/style-guide/eslint/browser'),
+    require.resolve('@vercel/style-guide/eslint/typescript'),
+    require.resolve('@vercel/style-guide/eslint/react'),
+    require.resolve('@vercel/style-guide/eslint/next'),
+    'plugin:tailwindcss/recommended',
+  ],
+  parserOptions: {
+    project,
+  },
+  settings: {
+    'import/resolver': { typescript: { project } },
+  },
+  globals: {
+    JSX: true,
+  },
+  rules: {},
+  overrides: [
+    {
+      // TypeScript
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': 'off', // naming is hard
+        '@typescript-eslint/non-nullable-type-assertion-style': 'off', // using as is more explicit
+        '@typescript-eslint/strict-boolean-expressions': 'error', // explicit is good
+      },
+    },
+    {
+      // Playwright
+      files: ['tests/**/*.spec.ts', 'tests/**/*.spec.tsx', '**/*.test.tsx'],
+      extends: [require.resolve('@vercel/style-guide/eslint/playwright-test')],
+    },
+  ],
+};
---
+++ README.md
@@ -0,0 +1,11 @@
+# todo spec
+
+A demo TODO app with a filesystem database. :scroll: For demonstrating testing!
+
+![Sneakpeek](docs/sneakpeek.png)
+
+Based on **TodoMVC**, except I changed the design a little so it is actually visible.
+
+## License
+
+MIT
