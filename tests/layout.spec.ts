ee900528987403848ea55c7be864eb37aef60b64
oreqizer <oreqizer@gmail.com>
2024-01-20 19:02:28 +0100

    simplify screenshot tests

--- tests/layout.spec.ts
+++ tests/layout.spec.ts
@@ -10,30 +10,6 @@ test.describe('visual', () => {
   test('screenshot', async ({ page }) => {
     await expect(page).toHaveScreenshot();
   });
-
-  test('screenshot heading', async ({ page }) => {
-    await expect(page.getByTestId('heading')).toHaveScreenshot();
-  });
-
-  test('screenshot content', async ({ page }) => {
-    await expect(page.getByTestId('content')).toHaveScreenshot();
-  });
-
-  test('screenshot input', async ({ page }) => {
-    await expect(page.getByTestId('input')).toHaveScreenshot();
-  });
-
-  test('screenshot todo list', async ({ page }) => {
-    await expect(page.getByTestId('todo-list')).toHaveScreenshot();
-  });
-
-  test('screenshot controls', async ({ page }) => {
-    await expect(page.getByTestId('controls')).toHaveScreenshot();
-  });
-
-  test('screenshot footer', async ({ page }) => {
-    await expect(page.getByTestId('footer')).toHaveScreenshot();
-  });
 });
 
 test.describe('visual empty', () => {
